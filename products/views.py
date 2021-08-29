ff33c452dee6e5b4942801c32b60f9593a42a766
emmasarge <emmasargeant206@gmail.com>
2021-08-29 10:39:23 +0000

    set up products.html

--- products/views.py
+++ products/views.py
@@ -3,11 +3,12 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view show all products to search and sort """
-    
+
     products = Product.objects.all()
-    
+
     context = {
         'products': products,
     }
