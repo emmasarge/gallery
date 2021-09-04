14c38d02d12491959be11b70b7e21a58e74611a3
emmasarge <emmasargeant206@gmail.com>
2021-09-04 08:25:58 +0000

    set up profile app

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,18 +11,18 @@ import stripe
 @csrf_exempt
 def webhook(request):
     """Listen for webhooks from Stripe"""
+    # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Get webhook data and signature
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
-            )
+        payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -48,6 +48,6 @@ def webhook(request):
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
 
-    # Call the event handler 
+    # Call the event handler with the event
     response = event_handler(event)
     return response
