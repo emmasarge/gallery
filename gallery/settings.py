22b90fe0d1db4887c7af0645e809abd569f116f4
emmasarge <emmasargeant206@gmail.com>
2021-09-04 15:30:49 +0000

    heroku deployment preparation

--- gallery/settings.py
+++ gallery/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,7 +14,7 @@ SECRET_KEY = 'P]FOiB+9&3z[kvp!~~)!wekjlVTD,2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-gallery-project.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -103,14 +104,26 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'gallery.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 DATABASES = {
-    'default': {
+    
+   'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
+}
 }
 
 
