34ba9ec0fc8f09afd1d499e3bf853b6ea3f4c1b7
emmasarge <emmasargeant206@gmail.com>
2021-09-05 10:45:59 +0000

    fixed database settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -118,14 +118,6 @@ else:
     }
 
 
-DATABASES = {
-      
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
-
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
