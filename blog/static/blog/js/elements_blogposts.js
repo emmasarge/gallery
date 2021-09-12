bcd5184cc3924f19c47da0b2a870d52291cc039c
emmasarge <emmasargeant206@gmail.com>
2021-09-12 21:08:13 +0000

    remove unused js files

--- blog/static/blog/js/elements_blogposts.js
+++ blog/static/blog/js/elements_blogposts.js
@@ -3,12 +3,3 @@ $('#new-image').change(function() {
     $('#filename').text(`Image will be set to: ${file.name}`);
 });
 
-// Animation on scroll
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
-sr.reveal('.post-header', {delay: 400});
-sr.reveal('.post-form', {delay: 600, interval: 200});
